55d40359248e85b6c83c045f8828e7b03fe784a3
Kaviya97 <40361033+Kaviya97@users.noreply.github.com>
2018-07-12 12:59:16 +0530

    Update positive or neg num.py

--- positive or neg num.py
+++ positive or neg num.py
@@ -1,8 +1,11 @@
 # To check whether the number is positive or negative
-    num=int(input("<"))
-    if(num<0):
-        print("negative")
-    elif(num==0):
-        print("zero")
-    else:
-        print("positive")
+num=int(input("<"))
+if(num<0):
+     print("negative")
+elif(num==0):
+     print("zero")
+else:
+     print("positive")
+
+    
+     
