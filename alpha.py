7ee9f80d168caf31700432928199de378121b9b4
Kaviya97 <40361033+Kaviya97@users.noreply.github.com>
2018-07-12 14:55:30 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,8 @@
+# To check whether the input is alphabet or not
+while True:
+    a=input(">")
+    if(64<ord(a)<91)or(95<ord(a)<123):
+            print("Alphabet")
+    else:
+            print('No')
+    
